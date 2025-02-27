9d9fe9cff279d1fe9cef3a49358cd8ab0dbdcba2
ViniciusGregorio <112400890+ViniciusGregorio@users.noreply.github.com>
2025-02-27 12:14:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('surpriseButton').addEventListener('click', function() {
+    // Mostra a mensagem de surpresa
     document.getElementById('surpriseText').classList.remove('hidden');
+
+    // Mostra o vídeo do YouTube
+    document.getElementById('videoContainer').classList.remove('hidden');
 });
